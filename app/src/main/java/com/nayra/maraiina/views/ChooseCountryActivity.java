b020991dad679a6133d24827e7b144e35355b5ce
nayrael-sayed <123nany>
2018-05-02 04:39:46 +0200

    fix spinner

--- app/src/main/java/com/nayra/maraiina/views/ChooseCountryActivity.java
+++ app/src/main/java/com/nayra/maraiina/views/ChooseCountryActivity.java
@@ -6,7 +6,11 @@ import android.view.View;
 import android.widget.Spinner;
 
 import com.nayra.maraiina.R;
+import com.nayra.maraiina.adapters.SpinnerCountryCustomAdapter;
+import com.nayra.maraiina.model.CountryModel;
 import com.nayra.maraiina.util.Utils;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,12 +21,28 @@ public class ChooseCountryActivity extends AppCompatActivity {
     @BindView(R.id.spCountries)
     Spinner countrySpinner;
 
+    private ArrayList<CountryModel> countryModels;
+
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_country);
 
         ButterKnife.bind(this);
+
+        countryModels = new ArrayList<>();
+        CountryModel c = new CountryModel();
+        c.setCountryName("AbuZabi");
+        countryModels.add(c);
+        c = new CountryModel();
+        c.setCountryName("Dubai");
+        countryModels.add(c);
+        c = new CountryModel();
+        c.setCountryName("Asharqah");
+        countryModels.add(c);
+
+        SpinnerCountryCustomAdapter countryCustomAdapter = new SpinnerCountryCustomAdapter(this, countryModels);
+        countrySpinner.setAdapter(countryCustomAdapter);
     }
 
     @OnClick(R.id.btnContinue) void continueToApp(final View view){
