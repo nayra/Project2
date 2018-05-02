b9529d4eaf3d96fde60f904e4b7267429df07996
nayrael-sayed <123nany>
2018-05-02 04:18:21 +0200

    fix drawer

--- app/src/main/java/com/nayra/maraiina/views/MainActivity.java
+++ app/src/main/java/com/nayra/maraiina/views/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Toast;
 
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
@@ -25,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     private CustomDrawerItem[] drawerItems = new CustomDrawerItem[6];
 
+    private CustomDrawerItem lastSelectedItem;
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +42,39 @@ public class MainActivity extends AppCompatActivity {
 
     private void initMenu() {
 
+        initAllMenuItems();
+
+        result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withHeader(R.layout.header_menu)
+                .withSliderBackgroundColor(ContextCompat.getColor(this, R.color.green_light))
+                .withHeaderDivider(true)
+                .withDisplayBelowStatusBar(true)
+                .addDrawerItems(
+                        drawerItems
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+
+                        if (drawerItem != lastSelectedItem) {
+                            drawerItem = setItemSelected((CustomDrawerItem) drawerItem);
+                            result.updateItem(drawerItem);
+
+                            lastSelectedItem = setItemNotSelected(lastSelectedItem);
+                            result.updateItem(lastSelectedItem);
+
+                            lastSelectedItem = (CustomDrawerItem) drawerItem;
+                        }
+                        result.closeDrawer();
+                        return true;
+                    }
+                })
+                .build();
+    }
+
+    private void initAllMenuItems() {
         final CustomDrawerItem item1 = initMenuItem(R.string.drawer_item_home, R.color.green_selected_menu, 1);
 
         final CustomDrawerItem item2 = initMenuItem(R.string.drawer_item_previous_orders, R.color.green_very_dark, 2);
@@ -61,35 +94,7 @@ public class MainActivity extends AppCompatActivity {
         drawerItems[4] = item5;
         drawerItems[5] = item6;
 
-        //create the drawer and remember the `Drawer` result object
-        result = new DrawerBuilder()
-                .withActivity(this)
-                .withToolbar(toolbar)
-                .withHeader(R.layout.header_menu)
-                .withSliderBackgroundColor(ContextCompat.getColor(this, R.color.green_light))
-                .withHeaderDivider(true)
-                .withDisplayBelowStatusBar(true)
-                .addDrawerItems(
-                        drawerItems
-                )
-                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-                    @Override
-                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-                        for (int i = 0; i < 6; i++) {
-                            if (i == (position - 1)) {
-                                drawerItems[i] = setItemSelected(drawerItems[i]);
-                                result.updateItemAtPosition(drawerItems[i], i);
-                            } else {
-                                drawerItems[i] = setItemNotSelected(drawerItems[i]);
-                                result.updateItemAtPosition(drawerItems[i], i);
-                            }
-                        }
-
-                        Toast.makeText(MainActivity.this, position + "", Toast.LENGTH_LONG).show();
-                        return true;
-                    }
-                })
-                .build();
+        lastSelectedItem = item1;
     }
 
     private CustomDrawerItem initMenuItem(int name, int color, int id) {
@@ -104,14 +109,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private CustomDrawerItem setItemNotSelected(CustomDrawerItem item) {
-        item.withBackgroundColor(R.color.green_very_dark);
+        item.withBackgroundColor(getResources().getColor(R.color.green_very_dark));
 
         return item;
     }
 
     private CustomDrawerItem setItemSelected(CustomDrawerItem item) {
-        item.withBackgroundColor(R.color.green_selected_menu);
-
+        item.withBackgroundColor(getResources().getColor(R.color.green_selected_menu));
+        item.withSetSelected(true);
         return item;
     }
 }
