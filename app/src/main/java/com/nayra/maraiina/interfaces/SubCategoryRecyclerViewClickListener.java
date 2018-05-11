d5f210f6940b9a5c0a8ed2e6239ecaa6a9b21b3f
nayrael-sayed <123nany>
2018-05-12 01:32:32 +0200

    interfaces

---
+++ app/src/main/java/com/nayra/maraiina/interfaces/SubCategoryRecyclerViewClickListener.java
@@ -0,0 +1,5 @@
+package com.nayra.maraiina.interfaces;
+
+public interface SubCategoryRecyclerViewClickListener {
+    void OnRecyclerViewClickListener(int subCatId);
+}
---
+++ app/src/main/java/com/nayra/maraiina/interfaces/WeightsRecyclerViewClickListener.java
@@ -0,0 +1,6 @@
+package com.nayra.maraiina.interfaces;
+
+public interface WeightsRecyclerViewClickListener {
+    void OnWeightsRecyclerViewClickListener(int pos);
+
+}
